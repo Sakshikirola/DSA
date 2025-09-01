37f9d354fb75770b5684608db56afa7532950bfe
Sakshi Kirola <kirolasakshi@gmail.com>
2025-09-01 21:39:54 +0530

    Time: 0 ms (100%), Space: 14.5 MB (49.7%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -0,0 +1,55 @@
+class Solution {
+private:
+    void dfs(int row, int col, vector<vector<int>>& vis, vector<vector<char>>& board) {
+        vis[row][col] = 1;
+        int n = board.size();
+        int m = board[0].size();
+        int drow[4] = {-1, 0, 1, 0};
+        int dcol[4] = {0, 1, 0, -1};
+
+        for (int i = 0; i < 4; i++) {
+            int nrow = row + drow[i];
+            int ncol = col + dcol[i];
+            if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+                !vis[nrow][ncol] && board[nrow][ncol] == 'O') {
+                dfs(nrow, ncol, vis, board);
+            }
+        }
+    }
+
+public:
+    void solve(vector<vector<char>>& board) {
+        int n = board.size();
+        int m = board[0].size();
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+
+        // 1. Traverse first and last row
+        for (int j = 0; j < m; j++) {
+            if (board[0][j] == 'O' && !vis[0][j]) {
+                dfs(0, j, vis, board);
+            }
+            if (board[n-1][j] == 'O' && !vis[n-1][j]) {
+                dfs(n-1, j, vis, board);
+            }
+        }
+
+        // 2. Traverse first and last column
+        for (int i = 0; i < n; i++) {
+            if (board[i][0] == 'O' && !vis[i][0]) {
+                dfs(i, 0, vis, board);
+            }
+            if (board[i][m-1] == 'O' && !vis[i][m-1]) {
+                dfs(i, m-1, vis, board);
+            }
+        }
+
+        // 3. Convert all unvisited 'O' to 'X'
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!vis[i][j] && board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+};
